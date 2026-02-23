70f784d8d527e781b1e54866db19319e34ea1142
Kayla Luo <kluo17@my.bcit.ca>
2026-02-23 13:50:36 -0800

    feat(nav): glass full-width nav, logo left, pill right, design tokens

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,50 +30,38 @@ export default function Navbar() {
     }, [pathname]);
 
     return (
-        <nav className='nav'>
-            <div className='container'>
+        <nav className='nav' aria-label='Main navigation'>
+            <div className='navInner'>
+                <Link className='navLogo' href='/' aria-label='Home'>
+                    <Image
+                        src='/images/main-logo.png'
+                        alt='Logo'
+                        width={56}
+                        height={56}
+                    />
+                </Link>
                 <div className='navPill'>
-                    <div className='navLeft'>
-                        <Link className='navIconButton' href='/' aria-label='Home'>
-                            <Image
-                                src='/images/logo.png'
-                                alt='Logo'
-                                width={36}
-                                height={36}
-                            />
+                        <Link
+                            href='/#featured-work'
+                            className={
+                                pathname === "/" && activeSection === "featured-work"
+                                    ? "navLink active"
+                                    : "navLink"
+                            }
+                        >
+                            Work
                         </Link>
-                        <ul className='navLinks'>
-                            <li>
-                                <Link
-                                    href='/#featured-work'
-                                    className={
-                                        pathname === "/" && activeSection === "featured-work"
-                                            ? "active"
-                                            : ""
-                                    }
-                                >
-                                    Work
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className={isActive("/about") ? "active" : ""} href='/about'>
-                                    About
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='navControls'>
-                        <button className='themeToggle' type='button' aria-label='Toggle theme'>
-                            <svg viewBox='0 0 24 24' aria-hidden='true'>
-                                <path d='M12 3a1 1 0 0 1 1 1v1.5a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1zm0 14a5 5 0 1 0 0-10 5 5 0 0 0 0 10zm8-5a1 1 0 0 1-1 1h-1.5a1 1 0 1 1 0-2H19a1 1 0 0 1 1 1zM6.5 12a1 1 0 0 1-1 1H4a1 1 0 1 1 0-2h1.5a1 1 0 0 1 1 1zm10.3-5.3a1 1 0 0 1 0-1.4l1.1-1.1a1 1 0 1 1 1.4 1.4l-1.1 1.1a1 1 0 0 1-1.4 0zM5.8 18.2a1 1 0 0 1 0-1.4l1.1-1.1a1 1 0 0 1 1.4 1.4l-1.1 1.1a1 1 0 0 1-1.4 0zm12.5 0a1 1 0 0 1-1.4 0l-1.1-1.1a1 1 0 1 1 1.4-1.4l1.1 1.1a1 1 0 0 1 0 1.4zM7.2 6.1a1 1 0 0 1-1.4 0L4.7 5a1 1 0 1 1 1.4-1.4l1.1 1.1a1 1 0 0 1 0 1.4zM12 19a1 1 0 0 1 1 1v1.5a1 1 0 1 1-2 0V20a1 1 0 0 1 1-1z' />
-                            </svg>
-                        </button>
+                        <Link
+                            className={isActive("/about") ? "navLink active" : "navLink"}
+                            href='/about'
+                        >
+                            About
+                        </Link>
                         <Link className='resumeButton' href='/resume'>
                             Resume
                             <span aria-hidden='true'>↗</span>
                         </Link>
                     </div>
-                </div>
             </div>
         </nav>
     );
