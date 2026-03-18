ffecb5675882c356e5f023007f837a76ee807706
Kayla Luo <kluo17@my.bcit.ca>
2026-03-18 04:08:17 -0700

    styles: connected daybreak to caseStudy.module

--- app/projects/daybreak/page.jsx
+++ app/projects/daybreak/page.jsx
@@ -2,7 +2,7 @@
 
 import daybreak from "../../../data/caseStudies/daybreak";
 import CaseStudyPage from "../../../components/case-study/CaseStudyPage";
-import styles from "./daybreak.module.css";
+import styles from "../../../styles/caseStudy.module.css";
 
 export default function DaybreakPage() {
     return <CaseStudyPage project={daybreak} styles={styles} />;
