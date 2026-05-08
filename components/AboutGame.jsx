39f1996abda687f278aef86d61e8dfac89fc0121
Kayla Luo <kluo17@my.bcit.ca>
2026-05-08 02:06:13 -0700

    add CSS Variables

--- components/AboutGame.jsx
+++ components/AboutGame.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useRef, useState, useCallback } from 'react';
-components/AboutGame.jsx
+import './AboutGame.css';
 
 const FACTS = [
     { icon: '✈️', text: 'I love traveling and exploring new countries.' },
